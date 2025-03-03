487191db3de079bdabe177927316fb557c0614a4
Hirtheesh1 <150221763+Hirtheesh1@users.noreply.github.com>
2025-03-03 22:12:18 +0530

    database connection estabilished

--- server/server.js
+++ server/server.js
@@ -5,13 +5,13 @@ const app = express();
 const port = 3000;
 
 // Import db.js to connect to MongoDB
- // Add this line
+require('./models/db')// Add this line
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-app.use('/', router);
+app.use('/user', router);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
